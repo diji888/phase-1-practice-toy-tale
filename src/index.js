ffbbd24890e000ba349e8c07db52d420d247767e
Diana Jiang <jiang99diana@gmail.com>
2021-11-09 21:54:46 -0500

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 let addToy = false;
+//try to only prepare variables before the DOMContentLoader
 
-document.addEventListener("DOMContentLoaded", () => {
+const f = fetch('http://localhost:3000/toys')
+console.log(f);
+
+ document.addEventListener("DOMContentLoaded", () => {
+
+  //fetch
+
   const addBtn = document.querySelector("#new-toy-btn");
+
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
@@ -12,4 +20,12 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  //Checking to see I can access the DOM
+  const toyCollectionDiv = document.querySelector("#toy-collection");
+  const tempH1 = document.createElement("h1");
+  tempH1.textContent = "Yay!"
+  
+  toyCollectionDiv.append(tempH1)
+
 });
