c669824ba2a75025bfd62ad8aa10bc62c29a618b
Diana Jiang <jiang99diana@gmail.com>
2021-11-09 23:24:48 -0500

    First commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 let addToy = false;
 //try to only prepare variables before the DOMContentLoader
 
-const f = fetch('http://localhost:3000/toys')
-console.log(f);
+
 
  document.addEventListener("DOMContentLoaded", () => {
 
+  //Checking to see I can access the DOM
+  const toyCollectionDiv = document.querySelector("#toy-collection");
+  const tempH1 = document.createElement("h1");
+  tempH1.textContent = "Yay!"
+  
+  toyCollectionDiv.append(tempH1)
+  
   //fetch
+  let theStateOfOurFetchData = [] 
+
+  console.log("This is the array before the fetch: ", theStateOfOurFetchData);
+
+const f = fetch("http://localhost:3000/toys")
+  .then( response => response.json() )
+  .then( (fetchData) => {
+    
+    console.log("This is what we got back", fetchData);
+
+    theStateOfOurFetchData = fetchData
+
+    console.log("This is the array after the fetch",theStateOfOurFetchData);
+
+    let oneToy = theStateOfOurFetchData[0];
+    console.log("This is the first toy's info", oneToy);
+
+    const oneToyOnTheDOM = document.createElement("h1");
+    oneToyOnTheDOM.textContent = "Yay!";
+
+    toyCollectionDiv.append(oneToyOnTheDOM);
+  })
+
+
+
+
+
+
 
   const addBtn = document.querySelector("#new-toy-btn");
 
@@ -21,11 +55,6 @@ console.log(f);
     }
   });
 
-  //Checking to see I can access the DOM
-  const toyCollectionDiv = document.querySelector("#toy-collection");
-  const tempH1 = document.createElement("h1");
-  tempH1.textContent = "Yay!"
-  
-  toyCollectionDiv.append(tempH1)
+
 
 });
